6ea078b441588bc79618a8319abfba29ba3773ea
TangMuSen <13717649868@163.com>
2023-06-15 15:49:51 +0800

    save

--- src/App.js
+++ src/App.js
@@ -18,3 +18,39 @@ export function createSphereSkybox(scene) {
 
   scene.add(sphere)
 }
+
+export function initRenderer() {
+  const canvas = document.createElement('canvas')
+  document.body.appendChild(canvas)
+  const renderer = new THREE.WebGLRenderer({ canvas })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  return renderer
+}
+
+export function initCamera() {
+  const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000) // 创建透视相机
+  camera.position.set(2,2,2)
+  const target = new THREE.Vector3(0, 0.3, 0);
+  camera.lookAt(target)
+  return camera
+}
+
+export function initLight(scene) {
+  const directionaLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionaLight.position.set(10, 10, 0);
+  directionaLight.castShadow = true;
+  directionaLight.shadow.mapSize.width = 1024;
+  directionaLight.shadow.mapSize.height = 1024;
+  directionaLight.shadow.camera.near = 0.1;
+  directionaLight.shadow.camera.far = 50;
+  directionaLight.shadow.radius = 10
+  directionaLight.shadow.bias = -0.001;
+  scene.add( directionaLight );
+
+  const ambientLight = new THREE.AmbientLight( 0x404040, 2 ); // 柔和的白光
+  scene.add( ambientLight );  
+
+  return { directionaLight, ambientLight }
+}
--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,16 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from 'three/examples/jsm/controls/DragControls.js'
 import { GUI } from 'dat.gui'
-import { createSphereSkybox } from '@/App.js'
+import { createSphereSkybox, initRenderer, initCamera, initLight } from '@/App.js'
 import { modelsImport } from '@/lib/tool.js'
-const gui = new GUI()
+// const gui = new GUI()
+let mixer = null
+const target = new THREE.Vector3(0, 0.3, 0);
 
 const scene = new THREE.Scene()
-const canvas = document.createElement('canvas')
-document.body.appendChild(canvas)
-const renderer = new THREE.WebGLRenderer({ canvas })
-
-let mixer = null
+const renderer = initRenderer()
+const camera = initCamera(target)
+const { directionaLight, ambientLight } = initLight(scene)
 
 modelsImport('models/dog/source/animation-10-Rover.glb', 'models/fairy_forest.glb').then(([dog, forest]) => {
   dogLoadSuccess(dog)
@@ -26,17 +26,6 @@ modelsImport('models/dog/source/animation-10-Rover.glb', 'models/fairy_forest.gl
 createSphereSkybox(scene)
 
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000) // 创建透视相机
-camera.position.set(2,2,2)
-var target = new THREE.Vector3(0, 0.25, 0);
-camera.lookAt(target)
-
-
-// const cylinderGeometry = new THREE.CylinderGeometry( 2, 2, 40, 8, 12 );
-// const material = new THREE.MeshPhongMaterial({skinning: true});
-// const mesh = new THREE.SkinnedMesh(cylinderGeometry, material)
-// scene.add( mesh );
-
 function dogLoadSuccess(gltf) {
   var model = gltf.scene;
   model.scale.set(0.3,0.3,0.3)
@@ -45,19 +34,9 @@ function dogLoadSuccess(gltf) {
   model.traverse(function (child) {
     if (child instanceof THREE.Mesh) {
       child.castShadow = true; 
-      // child.material = new THREE.MeshStandardMaterial({  wireframe: false });
-      child.receiveShadow = true; // 模型接收阴影
+      child.receiveShadow = true;
     }
   });
-
-  // const dcontrols = new DragControls( model, camera, renderer.domElement );
-  // dcontrols.addEventListener( 'dragstart', function ( event ) {
-  //   event.object.material.emissive.set( 0xaaaaaa );
-  //   console.log(event);
-  // } );
-  // dcontrols.addEventListener( 'dragend', function ( event ) {
-  //   event.object.material.emissive.set( 0x000000 );
-  // } );
 
   const animations = gltf.animations
   mixer = new THREE.AnimationMixer(model);
@@ -71,9 +50,9 @@ function dogLoadSuccess(gltf) {
 function forestLoadSuccess(gltf) {
   var model = gltf.scene;
   model.position.set(-0.72,-0.83,0)
-  gui.add(model.position, 'x', -10, 10, 0.01)
-  gui.add(model.position, 'y', -10, 10, 0.01)
-  gui.add(model.position, 'z', -10, 10, 0.01)
+  // gui.add(model.position, 'x', -10, 10, 0.01)
+  // gui.add(model.position, 'y', -10, 10, 0.01)
+  // gui.add(model.position, 'z', -10, 10, 0.01)
   scene.add(model);
 
   model.traverse(function (child) {
@@ -85,32 +64,9 @@ function forestLoadSuccess(gltf) {
   });
 }
 
-var directionaLight = new THREE.DirectionalLight(0xffffff, 1);
-directionaLight.position.set(10, 10, 0);
-directionaLight.castShadow = true; // 启用阴影投射
-scene.add( directionaLight );  
-
-const light = new THREE.AmbientLight( 0x404040, 2 ); // 柔和的白光
-scene.add( light );  
-
-// const spotLight = new THREE.SpotLight( 0xffffff, 1 );
-// spotLight.position.set( 0, 5, 5 );
-// scene.add(spotLight);
-// spotLight.castShadow = true; // 开启阴影
-directionaLight.shadow.mapSize.width = 1024;
-directionaLight.shadow.mapSize.height = 1024;
-directionaLight.shadow.camera.near = 0.1;
-directionaLight.shadow.camera.far = 50;
-directionaLight.shadow.radius = 10
-directionaLight.shadow.bias = -0.001;
-// const canvas = document.createElement('canvas')
-// document.body.appendChild(canvas)
-// const renderer = new THREE.WebGLRenderer({ canvas }) // 创建渲染器
-renderer.setSize(window.innerWidth, window.innerHeight) // 设置渲染器尺寸
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.target = target
+
 var prevTime = 0;
 function run (currentTime) {
   var deltaTime = (currentTime - prevTime) / 1000; // 将毫秒转换为秒
@@ -120,9 +76,6 @@ function run (currentTime) {
   renderer.render(scene, camera) // 渲染
   requestAnimationFrame(run)
 }
-// run()
-
-// tool
 
 </script>
 
